7eb6c5d4d71f25c42d2a0534c38217d3b513217b
Dede Irwanto <52788483+dede-irwanto@users.noreply.github.com>
2021-10-31 17:35:44 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { NavbarComponent } from './components'
+import { Home, Sukses } from './pages'
+
+export class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <NavbarComponent />
+        <main>
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/sukses" component={Sukses} exact />
+          </Switch>
+        </main>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default App
